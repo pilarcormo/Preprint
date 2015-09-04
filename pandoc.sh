07fdea0f98d4937a3425202fac70d9306a2cffd6
pilarcormo <pilarcormo@gmail.com>
2015-09-04 17:34:41 +0100

    minor changes

--- pandoc.sh
+++ pandoc.sh
@@ -1,4 +1,4 @@
-pandoc --latex-engine=xelatex --latexmathml -H preamble.tex -V fontsize=12pt  -V papersize:a4paper -fmarkdown-implicit_figures output.md -o "final.pdf"
+pandoc --latex-engine=xelatex --latexmathml -H preamble.tex -V fontsize=12pt  -V papersize:a4paper -fmarkdown-implicit_figures output.md -o "output.pdf"
 
 
 pandoc --latex-engine=xelatex -H preamble.tex -V fontsize=11pt -V documentclass:article -V papersize:a4paper -V classoption:openright --bibliography=library2.bib --csl="csl/biomed-central.csl" Background.md -o "background.pdf"
